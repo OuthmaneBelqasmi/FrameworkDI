6a6087c9d1bab54dd10dcfb390a6070348915707
outhmane <outhmane@outhmane-PC>
2018-04-26 21:27:46 +0100

    FrameworkDI v 0.2

--- FrameworkDI/src/main/java/test/test.java
+++ FrameworkDI/src/main/java/test/test.java
@@ -11,8 +11,7 @@ public class test {
 		inject.Init("dao","metier");
 		IMetier metier = (IMetier) inject.getBean("IMetier");
 		System.out.println(metier.calcule());
-		System.out.println("com     ");
-
+		System.out.println("ddd   ");
 		
 
 	}
